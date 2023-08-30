83cd83e4ccdfc9fcf9638759cf46c6b9e634801a
NeoThePotato <neorayb@gmail.com>
2023-08-30 03:20:04 +0300

    Added framework for Gyro-controlled input.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class InputManager : MonoBehaviour
+{
+	[System.NonSerialized] public Vector3 tiltVector = Vector3.zero;
+
+	private void OnTilt(InputValue value)
+	{
+		var vec = value.Get<Vector3>();
+		tiltVector = vec;
+	}
+}
