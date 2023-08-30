9e5f89446439a20610d8c3a654df2d483a3fb093
NeoThePotato <neorayb@gmail.com>
2023-08-30 06:00:14 +0300

    Gyro is now working and integrated.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private InputManager _inputManager;
+	[SerializeField] private GameObject _gameBoard;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void FixedUpdate()
+	{
+		_gameBoard.transform.rotation = Quaternion.Euler(_inputManager.tiltVectorCumulative);
+	}
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,15 +1,37 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class InputManager : MonoBehaviour
 {
-	[System.NonSerialized] public Vector3 tiltVector = Vector3.zero;
+	[System.NonSerialized] public Vector3 tiltVectorDelta = Vector3.zero;
+	[System.NonSerialized] public Vector3 tiltVectorCumulative = Vector3.zero;
+
+	private void Start()
+	{
+		EnableGyroControls();
+	}
 
 	private void OnTilt(InputValue value)
 	{
-		var vec = value.Get<Vector3>();
-		tiltVector = vec;
+		var vec = NormalizeGyro(value.Get<Vector3>());
+		tiltVectorDelta = vec;
+		tiltVectorCumulative += vec;
+	}
+
+	public void ResetGyro()
+	{
+		tiltVectorCumulative = Vector3.zero;
+	}
+
+	private Vector3 NormalizeGyro(Vector3 gyroVector)
+	{
+		return new Vector3(gyroVector.x, gyroVector.z, gyroVector.y);
+	}
+
+	private static void EnableGyroControls()
+	{
+		InputSystem.EnableDevice(UnityEngine.InputSystem.Gyroscope.current);
 	}
 }
