d581f66b99030009b19b66af972d2b5bbdaf9bd5
NeoThePotato <neorayb@gmail.com>
2023-09-07 20:29:05 +0300

    Added code for detecting collisions and counting player score.

--- Assets/Scripts/Boulder.cs
+++ Assets/Scripts/Boulder.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Boulder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void FreezeFor(float seconds)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        throw new NotImplementedException();
     }
 }
