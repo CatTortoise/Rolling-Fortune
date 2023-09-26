ba4ded6c0c05acbe9323b281f4cd8f5de678aa32
NeoThePotato <neorayb@gmail.com>
2023-09-26 23:20:51 +0300

    Implemented Camera freezing on PauseMenu.

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,11 +8,15 @@ public class PauseManager : MonoBehaviour
 	[SerializeField] private GameObject _pauseMenu;
 	[SerializeField] private PlayerInput _playerInput;
 	[SerializeField] private GameObject[] _objectsToToggle;
+	[SerializeField] private Camera _camera;
+	private CameraClearFlags _cameraClearFlags;
+	private int _cameraCullingMask;
 	private InputActionMap _playerActionMap;
 	private bool _paused = false;
 
 	private void Start()
 	{
+		SaveCameraParameters();
 		_playerActionMap = _playerInput.actions.FindActionMap("Player");
 		_playerInput.actions.FindAction("Pause").Enable();
 		ForceUnPause();
@@ -26,6 +31,7 @@ public class PauseManager : MonoBehaviour
 	public void ForcePause()
 	{
 		_paused = true;
+		StartCoroutine(FreezeCam());
 		_inGameMenu.SetActive(false);
 		_pauseMenu.SetActive(true);
 		SetAllObjects(false);
@@ -38,6 +44,7 @@ public class PauseManager : MonoBehaviour
 		_inGameMenu.SetActive(true);
 		_pauseMenu.SetActive(false);
 		SetAllObjects(true);
+		UnFreezeCam();
 		_playerActionMap.Enable();
 	}
 
@@ -58,6 +65,25 @@ public class PauseManager : MonoBehaviour
 	private void ToggleState()
 	{
 		_paused = !_paused;
+	}
+
+	private IEnumerator FreezeCam()
+	{
+		_camera.clearFlags = CameraClearFlags.Nothing;
+		yield return null;
+		_camera.cullingMask = 0;
+	}
+
+	private void UnFreezeCam()
+	{
+		_camera.clearFlags = _cameraClearFlags;
+		_camera.cullingMask = _cameraCullingMask;
+	}
+
+	private void SaveCameraParameters()
+	{
+		_cameraClearFlags = _camera.clearFlags;
+		_cameraCullingMask = _camera.cullingMask;
 	}
 
 	private void OnApplicationFocus(bool focus)
