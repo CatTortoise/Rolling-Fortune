f5d55d5e84e5e1fef92d608e14f292b29544000b
MatrixVVhite <yotamoscohen@gmail.com>
2023-09-28 21:05:31 +0300

    Moved level assets

--- Assets/Scripts/LevelTransitions.cs
+++ Assets/Scripts/LevelTransitions.cs
@@ -10,12 +10,25 @@ public class LevelTransitions : MonoBehaviour
     private int currentLevel = 1;
     [SerializeField] private GameObject player;
     private static Vector3 playerStartingPos;
+    [SerializeField] private GameObject boardParent;
+    private const string RESOURCE_LEVEL_PATH = "Levels/Level ";
+    private GameObject tempLevelObject;
 
     public bool HaveAllGemsBeenCollected { get => _gems.Count == 0; }
 
     private void Start()
     {
         playerStartingPos = player.transform.position;
+        Destroy(GameObject.Find("Level " + currentLevel));
+        currentLevel++;
+        tempLevelObject = (GameObject)Resources.Load(RESOURCE_LEVEL_PATH + currentLevel);
+        if (tempLevelObject != null) { Debug.Log("Loaded object"); }
+        else { Debug.Log("Failed to load object"); }
+        Instantiate(tempLevelObject, boardParent.transform);
+        tempLevelObject.name = tempLevelObject.name.Replace("Level " + currentLevel + "(Clone)", "Level " + currentLevel);
+        tempLevelObject.SetActive(true);
+        tempLevelObject = null; //clearing temp object after use
+        player.transform.position = playerStartingPos;
     }
 
     public void GemCollected(Gem gem)
@@ -24,17 +37,20 @@ public class LevelTransitions : MonoBehaviour
         if (HaveAllGemsBeenCollected)
         {
             _escapeHatch.SetActive(true);
-            //Start loading the next level here
+            tempLevelObject = (GameObject)Resources.Load(RESOURCE_LEVEL_PATH + (currentLevel + 1));
+            if (tempLevelObject != null) { Debug.Log("Loaded object"); }
+            else { Debug.Log("Failed to load object"); }
         }
     }
 
     public void TransitionLevel()
     {
-        //Change transition to destroy and instantiate
+        Destroy(GameObject.Find("Level " + currentLevel));
         _escapeHatch.SetActive(false);
-        lvlFolders[currentLevel - 1].SetActive(false);
         currentLevel++;
-        lvlFolders[currentLevel - 1].SetActive(true);
+        Instantiate(tempLevelObject);
+        tempLevelObject.SetActive(true);
+        tempLevelObject = null; //clearing temp object after use
         player.transform.position = playerStartingPos;
         //reset score
     }
