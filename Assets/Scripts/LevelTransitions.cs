12d0f97bcc2f5be6b3e69b4469664f6e6efe7312
MatrixVVhite <yotamoscohen@gmail.com>
2023-09-28 21:25:24 +0300

    Finished level transition implementation

--- Assets/Scripts/LevelTransitions.cs
+++ Assets/Scripts/LevelTransitions.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class LevelTransitions : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     [SerializeField] private List<Gem> _gems;
     [SerializeField] private GameObject _escapeHatch;
     [SerializeField] private GameObject[] lvlFolders;
-    private int currentLevel = 1;
-    [SerializeField] private GameObject player;
-    private static Vector3 playerStartingPos;
+    [SerializeField] private GameObject currentLevel;
     [SerializeField] private GameObject boardParent;
+    private int currentLevelIndex = 1;
+    private static Vector3 playerStartingPos;
     private const string RESOURCE_LEVEL_PATH = "Levels/Level ";
     private GameObject tempLevelObject;
 
@@ -19,16 +20,6 @@ public class LevelTransitions : MonoBehaviour
     private void Start()
     {
         playerStartingPos = player.transform.position;
-        Destroy(GameObject.Find("Level " + currentLevel));
-        currentLevel++;
-        tempLevelObject = (GameObject)Resources.Load(RESOURCE_LEVEL_PATH + currentLevel);
-        if (tempLevelObject != null) { Debug.Log("Loaded object"); }
-        else { Debug.Log("Failed to load object"); }
-        Instantiate(tempLevelObject, boardParent.transform);
-        tempLevelObject.name = tempLevelObject.name.Replace("Level " + currentLevel + "(Clone)", "Level " + currentLevel);
-        tempLevelObject.SetActive(true);
-        tempLevelObject = null; //clearing temp object after use
-        player.transform.position = playerStartingPos;
     }
 
     public void GemCollected(Gem gem)
@@ -37,7 +28,7 @@ public class LevelTransitions : MonoBehaviour
         if (HaveAllGemsBeenCollected)
         {
             _escapeHatch.SetActive(true);
-            tempLevelObject = (GameObject)Resources.Load(RESOURCE_LEVEL_PATH + (currentLevel + 1));
+            tempLevelObject = (GameObject)Resources.Load(RESOURCE_LEVEL_PATH + (currentLevelIndex + 1));
             if (tempLevelObject != null) { Debug.Log("Loaded object"); }
             else { Debug.Log("Failed to load object"); }
         }
@@ -45,11 +36,11 @@ public class LevelTransitions : MonoBehaviour
 
     public void TransitionLevel()
     {
-        Destroy(GameObject.Find("Level " + currentLevel));
+        Destroy(currentLevel);
         _escapeHatch.SetActive(false);
-        currentLevel++;
-        Instantiate(tempLevelObject);
-        tempLevelObject.SetActive(true);
+        currentLevelIndex++;
+        currentLevel = Instantiate(tempLevelObject, boardParent.transform);
+        currentLevel.SetActive(true);
         tempLevelObject = null; //clearing temp object after use
         player.transform.position = playerStartingPos;
         //reset score
