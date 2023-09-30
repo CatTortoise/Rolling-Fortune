5b40e296beb637c02ed1618c506e3aaa8292e62e
MatrixVVhite <yotamoscohen@gmail.com>
2023-09-30 13:53:52 +0300

    Added textures and fixed gem collection

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -5,6 +5,7 @@ public class Gem : MonoBehaviour
     [SerializeField] private PlayerScoreScriptableObject _playerScore;
     [SerializeField] private int _scoreForPickup = 5;
     [SerializeField] private string _triggerByTag;
+    [SerializeField] private LevelTransitions _levelTransitionsRef;
 
     public int ScoreForPickup { get => _scoreForPickup;private set => _scoreForPickup = value; }
 
@@ -12,6 +13,7 @@ public class Gem : MonoBehaviour
     {
         if (other.gameObject.CompareTag(_triggerByTag))
         {
+            _levelTransitionsRef.GemCollected(this);
             gameObject.SetActive(false);
         }
     }
@@ -22,6 +24,4 @@ public class Gem : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
-
-
 }
