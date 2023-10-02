a30443e0a1531e59aeed1d3364b7243883ab8e68
NeoThePotato <neorayb@gmail.com>
2023-10-02 10:05:38 +0300

    Added Main Menu scene.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,39 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] private Player _player;
-    [SerializeField] private LevelTransitions _levelTransitions;
-    [SerializeField] private UnityAnalyticsManager _analytics;
+	[SerializeField] private LevelTransitions _levelTransitions;
+	[SerializeField] private UnityAnalyticsManager _analytics;
 
-    private void Update()
-    {
-        if (!_player.gameObject.activeSelf)
-        {
-            if(_player.CurrentLives > 0)
-            {
-                _player.Respond();
-            }
-            else
-            {
-                _analytics.LevelComplete(0, _player.CurrentLives, false);
-            }
-        }
-    }
+	private void Update()
+	{
+		if (!_player.gameObject.activeSelf)
+		{
+			if(_player.CurrentLives > 0)
+			{
+				_player.Respond();
+			}
+			else
+			{
+				_analytics.LevelComplete(0, _player.CurrentLives, false);
+			}
+		}
+	}
 
-    public static void ExitGame()
+	public static void StartGame()
+	{
+		SceneManager.LoadSceneAsync("Combined Levels", LoadSceneMode.Single);
+	}
+
+	public static void ExitToMainMenu()
+	{
+		SceneManager.LoadSceneAsync("Main Menu", LoadSceneMode.Single);
+	}
+
+	public static void ExitGame()
 	{
 		Application.Quit();
 	}
-
 }
