eb2854764c47fe001eb3e309c123c4c348e42ec0
NeoThePotato <neorayb@gmail.com>
2023-10-03 12:01:31 +0300

    Used DOTween to add some cool animations to "Gem".

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Gem : MonoBehaviour
 {
 	[SerializeField] private PlayerScoreScriptableObject _playerScore;
 	[SerializeField] private GemManager _gemManager;
+	[SerializeField] private MeshRenderer _renderer;
 	[SerializeField] private int _scoreForPickup = 5;
 	[SerializeField] private string _triggerByTag;
+	private Material _material;
 
 	public int ScoreForPickup { get => _scoreForPickup; }
+
+	private void Start()
+	{
+		_material = _renderer.material;
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
@@ -19,6 +27,13 @@ public class Gem : MonoBehaviour
 	{
 		_playerScore.RaiseScore(ScoreForPickup);
 		_gemManager.GemCollected(this);
-		gameObject.SetActive(false);
+		DODisappear();
+	}
+
+	private void DODisappear()
+	{
+		_material.DOColor(Color.gray, 0.25f);
+		transform.DOLocalRotate(new(0, 360, 0), 0.75f, RotateMode.FastBeyond360);
+		transform.DOScale(Vector3.zero, 0.75f).SetEase(Ease.InElastic, 0.5f).OnComplete(() => gameObject.SetActive(false));
 	}
 }
