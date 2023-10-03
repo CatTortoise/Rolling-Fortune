9fe8016f815aafc110b4ce6f1c2d82445db1628c
NeoThePotato <neorayb@gmail.com>
2023-10-03 12:24:22 +0300

    Added more neat idle animations to "Gem".

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -17,6 +17,11 @@ public class Gem : MonoBehaviour
 		_material = _renderer.material;
 	}
 
+	private void OnEnable()
+	{
+		DOIdle();
+	}
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.CompareTag(_triggerByTag))
@@ -30,8 +35,15 @@ public class Gem : MonoBehaviour
 		DODisappear();
 	}
 
+	private void DOIdle()
+	{
+		transform.DOLocalMoveY(0.5f, 1).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+		transform.DOLocalRotate(new(0, 360, 0), 5, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+	}
+
 	private void DODisappear()
 	{
+		transform.DOKill();
 		_material.DOColor(Color.gray, 0.25f);
 		transform.DOLocalRotate(new(0, 360, 0), 0.75f, RotateMode.FastBeyond360);
 		transform.DOScale(Vector3.zero, 0.75f).SetEase(Ease.InElastic, 0.5f).OnComplete(() => gameObject.SetActive(false));
