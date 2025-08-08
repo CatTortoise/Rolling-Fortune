ab9caf99bc3979f738386064e93bf5f74654dcd3
NeoThePotato <neorayb@gmail.com>
2025-08-09 00:26:54 +0300

    Create system for collection gems.

---
+++ Assets/Scripts/ECS/GemCollectionSystem.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Extensions;
+using Unity.Physics.Systems;
+using Animation;
+using static Physics.Extensions;
+
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateAfter(typeof(PhysicsSystemGroup))]
+[BurstCompile]
+public partial struct GemCollectionSystem : ISystem
+{
+	[BurstCompile]
+	public void OnUpdate(ref SystemState state)
+	{
+		state.Dependency = new CollectGemsJob()
+		{
+			ecb = SystemAPI.GetSingleton<EndFixedStepSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged),
+			world = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld
+		}.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
+	}
+
+	[BurstCompile]
+	public struct CollectGemsJob : ITriggerEventsJob
+	{
+		public EntityCommandBuffer ecb;
+		[ReadOnly] public PhysicsWorld world;
+
+		public void Execute(TriggerEvent triggerEvent)
+		{
+			var filterA = world.GetCollisionFilter(triggerEvent.BodyIndexA);
+			var filterB = world.GetCollisionFilter(triggerEvent.BodyIndexB);
+			var playerEntity = triggerEvent.EntityA;
+			var gemEntity = triggerEvent.EntityB;
+			if (BelongsTo(filterA, filterB, PLAYER_TAG, GEM_TAG, ref playerEntity, ref gemEntity))
+			{
+				ecb.RemoveComponent<PhysicsCollider>(gemEntity);
+				ecb.AddComponent<ScaleAnimation>(gemEntity, new() { animationSpeed = 2, targetLocal = 0f });
+			}
+		}
+	}
+}
