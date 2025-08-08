cb29a40fdcf54c0e21c690be50aa3771b04bc218
NeoThePotato <neorayb@gmail.com>
2025-08-09 02:48:32 +0300

    Fix Gem parenting.

---
+++ Assets/Scripts/ECS/ParentToBoardAuthoring.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace InGame
+{
+	public class ParentToBoardAuthoring : MonoBehaviour
+	{
+		public class Baker : Baker<ParentToBoardAuthoring>
+		{
+			public override void Bake(ParentToBoardAuthoring authoring)
+			{
+				var entity = GetEntity(TransformUsageFlags.Dynamic);
+				AddComponent<Parent>(entity, new() { Value = GetEntity(authoring.GetComponentInParent<RotateToInputAuthoring>(), TransformUsageFlags.Dynamic) });
+			}
+		}
+	}
+}
